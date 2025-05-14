6271950dd41e2e9c8f2319d5ea3f303bd5fa73de
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-14 22:08:17 +0530

    security configuration

---
+++ src/main/java/com/week5/SpringSecurity/configs/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.week5.SpringSecurity.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //tells spring - configuring spring security filter
+public class WebSecurityConfig {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/posts").permitAll()
+                        .requestMatchers("/posts/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService myInMemoryUserDetailsService() {
+        UserDetails normalUser = User
+                .withUsername("rahul")
+                .password(passwordEncoder().encode("rahul123"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/week5/SpringSecurity/services/UserService.java
@@ -0,0 +1,22 @@
+package com.week5.SpringSecurity.services;
+
+import com.week5.SpringSecurity.exceptions.ResourceNotFoundException;
+import com.week5.SpringSecurity.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+    final private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User - " + username + " not found"));
+    }
+}
