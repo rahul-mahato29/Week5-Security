4a81e4bd0ed70e7b36b3e3b845121aa8b14bd0cf
Rahul Mahato <mahatorahul2901@gmail.com>
2025-05-22 22:49:33 +0530

    added cookie

--- src/main/java/com/week5/SpringSecurity/controllers/AuthController.java
+++ src/main/java/com/week5/SpringSecurity/controllers/AuthController.java
@@ -5,6 +5,9 @@ import com.week5.SpringSecurity.dto.SingUpDTO;
 import com.week5.SpringSecurity.dto.UserDTO;
 import com.week5.SpringSecurity.services.AuthService;
 import com.week5.SpringSecurity.services.UserService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,8 +30,13 @@ public class AuthController {
     }
 
     @PostMapping(path = "/login")
-    public ResponseEntity<String> logIn(@RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<String> logIn(@RequestBody LoginDTO loginDTO, HttpServletRequest request, HttpServletResponse response) {
          String token = authService.logIn(loginDTO);
+
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+
         return ResponseEntity.ok(token);
     }
 }
